0b4f69af7697ce36609ed3ae823b872b062e34eb
Alex Marz <alexhmarz@gmail.com>
2020-12-17 17:49:51 -0500

    Failing For Some Reason

---
+++ lib/create.sql
@@ -0,0 +1,24 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive TEXT 
+);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "dark brown", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sargeant Brown", 19, "M", "Green", "Slimy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "Blue", "naive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 20, "M", "black", "aggressive", 0);
+
+
+/*selects all of the female bears and returns their name and age (FAILED - 1)*/
+SELECT bears.name, bears.age FROM bears WHERE sex = 'F';
+
+SELECT DISTINCT * FROM bears ORDER BY age DESC LIMIT 1;
